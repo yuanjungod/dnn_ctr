ff654b73257e04869e5ec07c16c716ff3de6b8c8
yuanjun <yuanjun@papayamobile.com>
2019-01-25 21:58:38 +0800

    tiny

--- model/AFM.py
+++ model/AFM.py
@@ -517,4 +517,4 @@ with torch.cuda.device(0):
               weight_decay=0.00002, use_fm=True, use_ffm=False).cuda()
     afm.fit(result_dict['index'], result_dict['value'], result_dict['label'],
             test_dict['index'], test_dict['value'], test_dict['label'], ealry_stopping=True, refit=False,
-            save_path='../data/model/afm.pkl')
+            save_path='../data/afm.pkl')
