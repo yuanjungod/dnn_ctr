b3fba803bb3a8f7db0a31d671fe80c54ad2e820a
yuanjun <yuanjun@papayamobile.com>
2019-01-26 10:02:30 +0800

    gbm

--- gbm_test.py
+++ gbm_test.py
@@ -13,14 +13,14 @@ test_dict = data_preprocess.read_criteo_data('./data/tiny_test_input.csv', './da
 X_train, y_train = result_dict['index'], result_dict['label']
 X_test, y_test = test_dict['index'], test_dict['label']
 
-gbm = lgb.LGBMClassifier(num_leaves=31, learning_rate=0.05, n_estimators=100)
+gbm = lgb.LGBMClassifier(num_leaves=40, learning_rate=0.1, n_estimators=100)
 gbm.fit(np.array(X_train), np.array(y_train), eval_set=[(np.array(X_test), np.array(y_test))],
         eval_metric='l1', early_stopping_rounds=5)
 print(sum(y_train)/len(y_train))
 
 
-print("train auc:", roc_auc_score(y_train, gbm.predict_proba(X_train)[:, 1]))
-print("test auc:", roc_auc_score(y_test, gbm.predict_proba(X_test)[:, 1]))
+print("train auc:", roc_auc_score(np.array(y_train), gbm.predict_proba(np.array(X_train))[:, 1]))
+print("test auc:", roc_auc_score(np.array(y_test), gbm.predict_proba(np.array(X_test))[:, 1]))
 
 
 
