b9cedd4ae63c9e05596a643fe5176e6852c61954
yuanjun <yuanjun@papayamobile.com>
2019-01-26 09:48:43 +0800

    gbm

---
+++ gbm_test.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+
+from utils import data_preprocess
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import GridSearchCV
+import lightgbm as lgb
+from sklearn.ensemble import GradientBoostingClassifier
+import numpy as np
+
+result_dict = data_preprocess.read_criteo_data('./data/tiny_train_input.csv', './data/category_emb.csv')
+test_dict = data_preprocess.read_criteo_data('./data/tiny_test_input.csv', './data/category_emb.csv')
+
+X_train, y_train = result_dict['index'], result_dict['label']
+X_test, y_test = test_dict['index'], test_dict['label']
+
+gbm = lgb.LGBMClassifier(num_leaves=31, learning_rate=0.05, n_estimators=100)
+gbm.fit(X_train, y_train, eval_set=[(X_test, y_test)], eval_metric='l1', early_stopping_rounds=5)
+print(sum(y_train)/len(y_train))
+
+
+print("train auc:", roc_auc_score(y_train, gbm.predict_proba(X_train)[:, 1]))
+print("test auc:", roc_auc_score(y_test, gbm.predict_proba(X_test)[:, 1]))
+
+
+
